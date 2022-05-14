f2a89515dab0c55a1c89735434f404e9604495d9
Tejas M R <totejasmr@gmail.com>
2022-05-14 18:38:02 +0530

    ES module not working, so rolled back

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
-export const transform = { '^.+\\.ts?$': 'ts-jest' };
-export const testEnvironment = 'node';
-export const testRegex = '/tests/.*\\.(test|spec)?\\.(ts|tsx)$';
-export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node'];
+module.exports = {
+  transform: {'^.+\\.ts?$': 'ts-jest'},
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
