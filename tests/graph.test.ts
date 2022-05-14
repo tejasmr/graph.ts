ddfd86857b2f7747af279b8bd9e525e186fcff6d
Tejas M R <totejasmr@gmail.com>
2022-05-14 22:17:29 +0530

    Added test for single node graph with self loop

--- tests/graph.test.ts
+++ tests/graph.test.ts
@@ -31,3 +31,19 @@ describe('testing creating of single node graph', () => {
         expect(graph.adjecencyMatrix).toEqual([[0]]);
     });
 });
+
+describe('testing creation of single node graph with self loop', () => {
+    const graph = new Graph(1, [{u: 0, v: 0, w: 1}]);
+    test('node count should be 0', () => {
+        expect(graph.nodeCount).toBe(1);
+    });
+    test('edge list should be empty', () => {
+        expect(graph.edgeList).toEqual([{u: 0, v: 0, w: 1}]);
+    });
+    test('adjecency list have one element which is [0]', () => {
+        expect(graph.adjecencyList).toEqual([[0]]);
+    });
+    test('adjecency matrix should have one element which is 1', () => {
+        expect(graph.adjecencyMatrix).toEqual([[1]]);
+    });
+});
