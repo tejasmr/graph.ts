9735c2bfefdaf261d31302a672225256d3978a99
Tejas M R <totejasmr@gmail.com>
2022-05-14 18:28:20 +0530

    Converted to ES module

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-  transform: {'^.+\\.ts?$': 'ts-jest'},
-  testEnvironment: 'node',
-  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-};
+export const transform = { '^.+\\.ts?$': 'ts-jest' };
+export const testEnvironment = 'node';
+export const testRegex = '/tests/.*\\.(test|spec)?\\.(ts|tsx)$';
+export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx', 'json', 'node'];
