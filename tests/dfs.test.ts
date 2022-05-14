5c130b078412af0ddfb9d21db483181ef5ec7b22
Tejas M R <totejasmr@gmail.com>
2022-05-14 23:25:36 +0530

    Added two graph test with one edge for dfs

--- tests/dfs.test.ts
+++ tests/dfs.test.ts
@@ -31,3 +31,13 @@ describe('testing two node graph with no edges', () => {
         expect(dfs(graph, Array(2).fill(false), 1)).toEqual([1]);
     });
 });
+
+describe('testing two node graph with one edge', () => {
+    const graph = new Graph(2, [{u: 0, v: 1, w: 1}]);
+    test('dfs starting from 0 should be [0, 1]', () => {
+        expect(dfs(graph, Array(2).fill(false), 0)).toEqual([0, 1]);
+    });
+    test('dfs starting from 1 should be [1]', () => {
+        expect(dfs(graph, Array(2).fill(false), 1)).toEqual([1]);
+    });
+})
