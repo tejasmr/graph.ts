e75912cc277a92559efd0d494dc945a17ad9ad11
Tejas M R <totejasmr@gmail.com>
2022-05-15 15:49:59 +0530

    Fixed visited multiple times same node

--- src/traversals/bfs.ts
+++ src/traversals/bfs.ts
@@ -7,15 +7,18 @@ export default function bfs(graph: Graph, startNode: number): number[] {
 
     const trav: number[] = [];
     const q: number[] = [];
-    q.push(startNode);
 
+    visited[startNode] = true;
+    q.push(startNode);
+    
     while(q.length > 0) {
         const node = q.shift();
-        visited[node] = true;
         trav.push(node);
         graph.adjecencyList[node].forEach((neighbour) => {
-            if(!visited[neighbour])
+            if(!visited[neighbour]) {
                 q.push(neighbour);
+                visited[neighbour] = true;
+            }
         });
     }
     return trav;
